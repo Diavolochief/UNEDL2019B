be74abad2f0319513545b994f5fef775c75b7ec7
Diavolochief <54989277+Diavolochief@users.noreply.github.com>
2019-11-25 10:12:53 -0600

    Add files via upload

---
+++ PV2doParcial/PV2doParcial/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Gomez Ponce Erick Alonso */
+/* 2do parcial*/
+using System;
+using System.Collections;
+
+namespace PV2doParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa la ecuacion "); 
+            String var1 = Console.ReadLine();
+            Stack paretnesis = new Stack();
+            for (int i = 0; i < var1.Length; ++i)
+            {
+                if ((var1[i] == '(') || (var1[i] == '{') || (var1[i] == '[')) 
+                {
+                    paretnesis.Push(var1[i]);//coloca el iterador al final de la pila
+                }
+                else if (paretnesis.Count > 0)
+                {//entonces si el parentesis esta vacio es falso
+                    switch (var1[i])
+                    {//switch case con el iterador del array
+                        case ']'://caso ]
+
+                            paretnesis.Pop();
+                            break;
+                        case '}'://caso}
+
+                            paretnesis.Pop();
+                            break;
+                        case ')'://caso )
+                            paretnesis.Pop();
+                            break;
+                    }//cierra switch case
+                }//cierra if else
+            }//cierra for
+            if (paretnesis.Count == 0)
+            {//si esta vacio entonces
+                Console.WriteLine("ecuacion correcta");//imprime es correcta
+            }
+            else
+            {//si no
+                Console.WriteLine("no esta equilibrada");//imprime no esta equilibrada
+            }//cierra else
+        }
+    }
+}
